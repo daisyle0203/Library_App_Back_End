e38e874d14ab11de645b1f0d2e962747a18f5bf0
Daisy Le <daisy.le0203@gmail.com>
2023-04-16 23:02:02 -0500

    add badic springboot setup

---
+++ spring-boot-library/src/main/java/com/daisyle/springbootlibrary/SpringBootLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.daisyle.springbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryApplication.class, args);
+	}
+
+}
